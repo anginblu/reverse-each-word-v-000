7003759049b4c626cb12d636e57fc512b4b90a07
Jacqueline Lee <anginblu@gmail.com>
2017-10-20 03:01:40 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,6 +1,7 @@
 def reverse_each_word(word="Hello there, and how are you?")
   array = word.split
   array.each do |word|
-    "#{word.reverse} "
+    index = array.index(word)
+    array[index] = "#{word.reverse} "
   end
 end
